84dbf19a48985675931094443896f2a046be8ffa
Thomas Verderesi <tverderesi@gmail.com>
2024-05-10 18:58:03 -0300

    chore: Update button styles and form URL in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,21 @@
 class ArticlesController < ApplicationController
-  def new
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 
   def create
-    puts params
-    render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
